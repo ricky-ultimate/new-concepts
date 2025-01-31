7463b131c7c6171dd77b372338a2f8e28e4e0932
Ricky Ultimate <rickyogigi@gmail.com>
2025-01-31 23:19:42 +0100

    chore: include missing code descriptions

--- src/find_maximum.py
+++ src/find_maximum.py
@@ -1,4 +1,7 @@
 from typing import List, Union
+"""
+Program to find the maximum value in a hybrid list on numbers
+"""
 
 def find_max(nums: List[Union[int, float]]) -> Union[int, float, None]:
     if nums:
--- src/reverse_list.py
+++ src/reverse_list.py
@@ -1,4 +1,7 @@
 from typing import List
+"""
+program to reverse the elements of a list
+"""
 
 def reverse_list(lst: List[Any]) -> Union[List[Any], None]:
     return lst[::-1] if lst else None
