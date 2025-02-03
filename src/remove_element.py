baf8f27077e1dd013a15682a25c6dbbcd3412169
Ricky Ultimate <rickyogigi@gmail.com>
2025-02-03 12:18:45 +0100

    feat: program to remove an item from a lisy

---
+++ src/remove_element.py
@@ -0,0 +1,10 @@
+from typing import List, Any, Optional
+"""
+Function goal: to remove an element from a list and return the list without the element.
+"""
+
+def remove_element(lst: List[Any], item: Any) -> Optional[List[Any]]:
+    if not lst: return None
+    for i in range(len(lst)):
+        if item == lst[i]:
+            return lst[0:i] + lst[i+1:]
